3dc2f2dc15af56b8ee262a1264788c49acda475b
Alessandro <alessandros@live.de>
2015-05-22 00:10:20 +0200

    DBController

--- WebCalendarApplication/src/application/Controller/DatabaseController.java
+++ WebCalendarApplication/src/application/Controller/DatabaseController.java
@@ -2,14 +2,16 @@ package application.Controller;
 
 /*Singleton DatabaseController */
 
-public class DatabaseController {
+public class DatabaseController 
+{
 
 	private static DatabaseController instance;
 
-	private DatabaseController() {
+	private DatabaseController() 
+	{
+		
 	}
 	
-
 	/* Thread-Safe Singleton */
 	public static synchronized DatabaseController getInstance() {
 		if (DatabaseController.instance == null) {
